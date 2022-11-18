e2f812409ca3c09bb43ae2b331b2ee96c9f8ea23
oussama belkhadir <osamaalpha1997@gmail.com>
2022-11-18 19:53:45 +0100

    may need some external functions

---
+++ tools/libclass.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libclass.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obelkhad <obelkhad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/18 15:49:37 by obelkhad          #+#    #+#             */
+/*   Updated: 2022/11/18 19:53:03 by obelkhad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBCLASS
+#define LIBCLASS
+
+#include <type_traits>
+#include <algorithm>
+#include <memory>
+
+namespace ft
+{
+
+
+}	//namespace ft
+#endif
