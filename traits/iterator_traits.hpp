a00036c6173d255aa60fe21e5b1ae2adc8415b59
oussama belkhadir <osamaalpha1997@gmail.com>
2022-11-22 22:13:50 +0100

    modifie iteretor traits

--- traits/iterator_traits.hpp
+++ traits/iterator_traits.hpp
@@ -6,44 +6,38 @@
 /*   By: obelkhad <obelkhad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 16:08:49 by obelkhad          #+#    #+#             */
-/*   Updated: 2022/11/13 16:41:19 by obelkhad         ###   ########.fr       */
+/*   Updated: 2022/11/22 20:43:09 by obelkhad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITERATOR_TRAITS_HPP
 #define ITERATOR_TRAITS_HPP
-
+#include <iterator>
 namespace ft
 {
-	template <class Iterator> struct iterator_traits
+	template <class Iterator>
+	struct iterator_traits
 	{
-		typedef typename Iterator::difference_type		difference_type;
-		typedef typename Iterator::value_type			value_type;
-		typedef typename Iterator::pointer				pointer;
-		typedef typename Iterator::reference			reference;
-		typedef typename Iterator::iterator_category	iterator_category;
+		typedef ptrdiff_t								difference_type;
+		typedef Iterator								value_type;
+		typedef Iterator*								pointer;
 	};
 
-	template <class T> struct iterator_traits<T*>
+	template <class T> 
+	struct iterator_traits<T*>
 	{
-		typedef ptrdiff_t							difference_type;
-		typedef T									value_type;
-		typedef T*									pointer;
-		typedef T&									refernece;
-		typedef std::random_access_iterator_tag		random_access_iterator_tag;
+		typedef ptrdiff_t								difference_type;
+		typedef T										value_type;
+		typedef T*										pointer;
 	};
 
-	template <class T> struct iterator_traits<const T*>
+	template <class T> 
+	struct iterator_traits<const T*>
 	{
-		typedef ptrdiff_t							difference_type;
-		typedef T									value_type;
-		typedef const T*							const_pointer;
-		typedef const T&							const_refernece;
-		typedef std::random_access_iterator_tag		random_access_iterator_tag;
+		typedef ptrdiff_t								difference_type;
+		typedef T										value_type;
+		typedef const T*								const_pointer;
+		typedef const T&								const_refernece;
 	};
 }	//namespace ft
-
-//exmeple : https://en.cppreference.com/w/cpp/iterator/iterator_traits
-
-
 #endif
