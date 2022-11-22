f877026760065e3afc36d1e9eb35389143459e77
oussama belkhadir <osamaalpha1997@gmail.com>
2022-11-22 22:16:17 +0100

    add being() end()

--- Containers/ft_vector.hpp
+++ Containers/ft_vector.hpp
@@ -6,7 +6,7 @@
 /*   By: obelkhad <obelkhad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 08:52:01 by obelkhad          #+#    #+#             */
-/*   Updated: 2022/11/22 12:48:00 by obelkhad         ###   ########.fr       */
+/*   Updated: 2022/11/22 20:40:43 by obelkhad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <memory>
 #include <iostream>
 #include <algorithm>
+#include "../iterators/iterator.hpp"
 
 namespace ft
 {
@@ -30,16 +31,19 @@ namespace ft
 		//|                         [ Member types ]                         |//
 		//+------------------------------------------------------------------+//
 
-	private:
+	public:
 		typedef _Tp     	                                value_type;
 		typedef	_Allocator									allocator_type;
-		typedef typename allocator_type::pointer            pointer;            //typdef *_Tp pointer   
-		typedef typename allocator_type::reference		    reference;          //typdef &_Tp reference 
-		typedef typename allocator_type::const_pointer      const_pointer;      //typdef const *_Tp const_pointer    
-		typedef typename allocator_type::const_reference	const_reference;    //typdef const &_Tp const_reference   
+		typedef typename allocator_type::pointer            pointer;
+		typedef typename allocator_type::reference		    reference;
+		typedef typename allocator_type::const_pointer      const_pointer;
+		typedef typename allocator_type::const_reference	const_reference;
 		typedef typename allocator_type::size_type          size_type;
 		typedef typename allocator_type::difference_type    difference_type;
+		typedef	base_Iterator<pointer>						iterator;
+		typedef	base_Iterator<const_pointer>				const_iterator;
 
+	private:
 		pointer                                         __begin_;
 		pointer                                         __end_;
 		pointer                                         __end_cap_; //end of the allocated memory
@@ -91,18 +95,6 @@ namespace ft
 		}
 
 
-template <class _Tp, _Tp __v>
-struct  integral_constant
-{
-    static const _Tp      value = __v;
-    typedef _Tp               value_type;
-    typedef integral_constant type;
-    operator value_type() const 
-	{return value;}
-    constexpr value_type operator ()() const {return value;}
-
-};
-
 // vector(
 // 		_InputIterator __first,
 // 		_InputIterator __last,
@@ -117,15 +109,15 @@ struct  integral_constant
 // 		)
 
 		/* Range constructor */
-		template <class InputIterator>
-		vector (InputIterator first, InputIterator last, const allocator_type &alloc = allocator_type(),
-			typename ft::enable_if<!ft::is_integral<InputIterator>::value>::type* = 0)
-		:	__begin_(nullptr), __end_(nullptr), __end_cap_(nullptr), __alloc(alloc)
-		{
-			std::cout << "range container\n";
-			for (; first != last; ++first)
-        		push_back(*first);
-		}
+		// template <class InputIterator>
+		// vector (InputIterator first, InputIterator last, const allocator_type &alloc = allocator_type(),
+		// 	typename ft::enable_if<!ft::is_integral<InputIterator>::value>::type* = 0)
+		// :	__begin_(nullptr), __end_(nullptr), __end_cap_(nullptr), __alloc(alloc)
+		// {
+		// 	std::cout << "range container\n";
+		// 	for (; first != last; ++first)
+        // 		push_back(*first);
+		// }
 
 		/* Copy constructor */
 		vector(const vector &__x)
@@ -158,6 +150,20 @@ struct  integral_constant
 
 			/* deallocate */
 			__alloc.deallocate(__begin_, capacity());
+		}
+		//+------------------------------------------------------------------+//
+		//|    	                       [ Iterator ]                     	 |//
+		//+------------------------------------------------------------------+//
+		
+		iterator begin()
+		{
+			// iterator(__p);
+			return (__make_iter(__begin_));
+		}
+		
+		iterator end()
+		{
+			return (__make_iter(__end_));
 		}
 		//+------------------------------------------------------------------+//
 		//|    	                       [ Capacity ]                     	 |//
@@ -213,7 +219,7 @@ struct  integral_constant
 		{ __destruct_at_end(__begin_); }
 
 		//----------------------------------------------------------- [ insert ]
-		iterator insert (const_iterator __position, const_reference __val)
+		iterator insert (iterator __position, const_reference __val)
 		{
 			pointer __p = __begin_ + (__position - begin());
 			if (__end_ < __end_cap_)
@@ -236,12 +242,12 @@ struct  integral_constant
 				__v.push_back(__val);
 				__p = __construct_backward_and_swap_(__v, __p);
 			}
-			// return __make_iter(__p);
+			return __make_iter(__p);
 		}
 
-		insert(const_iterator __position, size_type __n, const_reference __val)
+		iterator insert(iterator __position, size_type __n, const_reference __val)
 		{
-			pointer __p = this->__begin_ + (__position - begin());
+			pointer __p = __begin_ + (__position - begin());
 			if (__n > 0)
 			{
 				if (__n <= static_cast<size_type>(__end_cap_ - __end_))
@@ -256,7 +262,7 @@ struct  integral_constant
 					}
 					if (__n > 0)
 					{
-						__move_range(__p, __old_last, __p + __old_n);
+						__shift_right(__p, __old_last, __p + __old_n);
 						const_pointer __pointer_ = &__val;
 						if (__p <= __pointer_ && __pointer_ < __end_)
 							__pointer_ += __old_n;
@@ -266,18 +272,18 @@ struct  integral_constant
 				else
 				{
 					__split_buffer __v(__recommend(size() + __n), __p - __begin_, __alloc);
-					__v.__construct_at_end(__n, __val);
+					__v.vec.__construct_at_end(__n, __val);
 					__p = __construct_backward_and_swap_(__v, __p);
 				}
 			}
-			// return __make_iter(__p);
+			return __make_iter(__p);
 		}
 
-		template <class InputIterator>
-		void insert (iterator position, InputIterator first, InputIterator last)
-		{
+		// template <class InputIterator>
+		// void insert (iterator position, InputIterator first, InputIterator last)
+		// {
 			
-		}
+		// }
 
 		//------------------------------------------------------------ [ erase ]
 		iterator erase(iterator __position)
@@ -326,23 +332,23 @@ struct  integral_constant
 			}
 		};
 
-		void __construct_backward_and_swap_(__split_buffer &__v, pointer __p);
+		pointer __construct_backward_and_swap_(__split_buffer &__v, pointer __p);
 		void __construct_backward_and_swap_(__split_buffer &__v);
 		void __append(size_type __n, const_reference __val);
 		size_type __recommend(size_type __new_size) const;
 		void __construct_at_end(size_type _n, const_reference _val);
 		inline void __destruct_at_end(pointer __new_last);
-		void __shift_right(pointer __pos, pointer __end_holder);
+		void __shift_right(pointer __pos, pointer __end_holder, pointer __p);
 		inline void move_backward(pointer _first, pointer _last, pointer _result);
+		inline iterator __make_iter(pointer __p);
 	};	// -------------------------------------------------   [ VECTOR ] ----//
 
 
 
 
-
-
 	template<class _Tp, class _Allocator>
-	void vector<_Tp, _Allocator>::__construct_backward_and_swap_(__split_buffer &__v, pointer __p)
+	typename vector<_Tp, _Allocator>::pointer 
+	vector<_Tp, _Allocator>::__construct_backward_and_swap_(__split_buffer &__v, pointer __p)
 	{
 		/* Construct Forward */
 		pointer __ptr = __p;
@@ -419,23 +425,30 @@ struct  integral_constant
 			__alloc.destroy(--__end_);
 	}
 
-	template <class _Tp, class _Allocator>
-	void vector<_Tp, _Allocator>::__shift_right(pointer __pos, pointer __end_holder)
-    {
-        pointer __old_last = __end_;
-        difference_type _n = __end_ - (__pos + 1);
-
-        for (pointer __p = __pos + _n; __p < __end_holder; ++__p, ++__end_)
-            __alloc.construct( __end_, *__p);
-        ft::__move_backward(__pos, __pos + _n, __old_last);
-    }
-	
 	template<class _Tp, class _Allocator>
 	inline void vector<_Tp, _Allocator>::move_backward(pointer _first, pointer _last, pointer _result)
     {
         while (_first != _last)
             *--_result = *--_last;
     }
+	
+	template <class _Tp, class _Allocator>
+	void vector<_Tp, _Allocator>::__shift_right(pointer __pos, pointer __end_holder, pointer __p)
+    {
+        pointer __old_last = __end_;
+        difference_type _n = __end_ - __p;
+
+        for (pointer __p = __pos + _n; __p < __end_holder; ++__p, ++__end_)
+            __alloc.construct( __end_, *__p);
+        move_backward(__pos, __pos + _n, __old_last);
+    }
+	
+	template <class _Tp, class _Allocator>
+	inline typename vector<_Tp, _Allocator>::iterator
+	vector<_Tp, _Allocator>::__make_iter(pointer __p)
+	{
+		return iterator(__p);
+	}
 
 }	//namespace ft
 #endif
