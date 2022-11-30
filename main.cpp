b94d306a9265f1ac1122ee0784214d609b0fba4d
oussama belkhadir <osamaalpha1997@gmail.com>
2022-11-30 18:08:03 +0100

    create vector test

--- main.cpp
+++ main.cpp
@@ -6,65 +6,39 @@
 /*   By: obelkhad <obelkhad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 14:55:39 by obelkhad          #+#    #+#             */
-/*   Updated: 2022/11/22 12:43:49 by obelkhad         ###   ########.fr       */
+/*   Updated: 2022/11/30 17:55:17 by obelkhad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <iostream>
-#include <vector>
-#include <list>
-// #include "Containers/ft_vector.hpp"
-
-#include <iterator>
+#include "Containers/ft_vector.hpp"
+#include "test.hpp"
 #include <iostream>
+#include <string>
+#include <vector>
+#include <type_traits>
 
-void p(std::vector<int>::iterator itr1, std::vector<int>::iterator itr2)
-{
-	while (itr1 != itr2)
-		std::cout << *itr1++ << ' ';
- 	std::cout << '\n';
-	
+
+int main ()
+{	
+	int test;
+	std::cout << "STD is_ integral<int> : " << std::is_integral<int>::value;
+	void (*vectorTest[18])() = {v_test0, v_test1, v_test2, v_test3, v_test4, v_test5, v_test6, v_test7, v_test8, v_test9, v_test10, v_test11, v_test12, v_test13, v_test14, v_test15, v_test16, v_test17};
+	while (1)
+	{
+		std::cout << "there are 1 to 17 tests vector, please choose One at time." << std::endl;
+		std::cout << "\033[36m" << "[ *** ] please compaire { STD OUTPUT} & { FT OUTPUT }, should be the same !!!" << "\033[0m" << std::endl;
+		std::cout << "test number : "; 
+		std::cin >> test;
+		if (test >= 0 && test <= 17)
+		{
+			system("clear");
+			vectorTest[test]();
+		}
+		else
+			std::cout << "\033[31m"  << "error : range form [1 - 16]" << "\033[0m" << std::endl;
+	}
+    
+    return (0);
 }
-void pl(std::list<int>::iterator itr1, std::list<int>::iterator itr2)
-{
-	while (itr1 != itr2)
-		std::cout << *itr1++ << ' ';
- 	std::cout << '\n';
-}
-int main()
-{
-	std::vector<int> v;
-	v.push_back(1);
-	v.push_back(2);
-	v.push_back(3);
-	v.push_back(4);
-	v.push_back(5);
-	v.push_back(6);
-	v.push_back(7);
-	v.push_back(8);
-	v.push_back(9);
-	v.push_back(10);
-	v.push_back(11);
-	v.push_back(12);
-	v.push_back(13);
-	v.push_back(14);
- 
- 
-    p(v.begin(), v.end());
- 	std::cout << "------------------------" << '\n';
 
-	
-	std::vector<int>::iterator itr1 = v.begin() + 3;
-	std::vector<int>::iterator itr2 = v.begin() + 8;
-	std::vector<int>::iterator itr3;
- 	std::cout << *itr1 << '\n';
- 	std::cout << *itr2 << '\n';
 
-	
- 	std::cout << "------------------------" << '\n';
-	itr3 = v.erase(itr1, itr2);
-
-    p(v.begin(), v.end());
- 	std::cout << *itr3 << '\n';
-
-}
