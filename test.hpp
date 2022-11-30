b6cb011d26590c484d0e66fa6e4f137706b88095
oussama belkhadir <osamaalpha1997@gmail.com>
2022-11-30 18:05:48 +0100

    add header

---
+++ test.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obelkhad <obelkhad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/27 19:29:44 by obelkhad          #+#    #+#             */
+/*   Updated: 2022/11/30 17:55:28 by obelkhad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef TEST_H
+#define TEST_H
+
+#include "Containers/ft_vector.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+
+void v_test0();
+void v_test1();
+void v_test2();
+void v_test3();
+void v_test4();
+void v_test5();
+void v_test6();
+void v_test7();
+void v_test8();
+void v_test9();
+void v_test10();
+void v_test11();
+void v_test12();
+void v_test13();
+void v_test14();
+void v_test15();
+void v_test16();
+void v_test17();
+#endif
