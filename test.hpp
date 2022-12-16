96cd6c15761512f84b760330bd05db52057ff0dc
oussama belkhadir <osamaalpha1997@gmail.com>
2022-12-16 23:37:35 +0100

    add stack tests

--- test.hpp
+++ test.hpp
@@ -6,7 +6,7 @@
 /*   By: obelkhad <obelkhad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/27 19:29:44 by obelkhad          #+#    #+#             */
-/*   Updated: 2022/11/30 17:55:28 by obelkhad         ###   ########.fr       */
+/*   Updated: 2022/12/02 17:45:12 by obelkhad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,14 @@
 #define TEST_H
 
 #include "Containers/ft_vector.hpp"
+#include <vector>
+#include "Containers/ft_stack.hpp"
+#include <stack>
+#include "Containers/ft_map.hpp"
+#include <map>
+#include <list>
 #include <iostream>
 #include <string>
-#include <vector>
 
 void v_test0();
 void v_test1();
@@ -37,4 +42,23 @@ void v_test14();
 void v_test15();
 void v_test16();
 void v_test17();
+
+void stk_test0();
+void stk_test1();
+void stk_test2();
+void stk_test3();
+void stk_test4();
+// void stk_test5();
+// void stk_test6();
+// void stk_test7();
+// void stk_test8();
+// void stk_test9();
+// void stk_test10();
+// void stk_test11();
+// void stk_test12();
+// void stk_test13();
+// void stk_test14();
+// void stk_test15();
+// void stk_test16();
+// void stk_test17();
 #endif
