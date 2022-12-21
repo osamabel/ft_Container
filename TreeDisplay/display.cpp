2609bf292049c7d7de7a5366d1402cca232d0637
oussama belkhadir <osamaalpha1997@gmail.com>
2022-12-21 18:44:25 +0100

    display tree last version

--- TreeDisplay/display.cpp
+++ TreeDisplay/display.cpp
@@ -6,7 +6,7 @@
 /*   By: obelkhad <obelkhad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 15:36:05 by obelkhad          #+#    #+#             */
-/*   Updated: 2022/12/17 20:33:01 by obelkhad         ###   ########.fr       */
+/*   Updated: 2022/12/21 18:43:11 by obelkhad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 #include <iomanip>
 #include <locale> 
 #include "../Containers/ft_map.hpp"
+#include "../Tree/tree_iterator.hpp"
+
 
 typedef ft::map<int, char>::node_pointer   			node_pointer;
 typedef ft::map<int, char>::node					node;
@@ -25,67 +27,43 @@ typedef struct s_data{
 
 	node_pointer	__node;				//	node
 	char			__direction;		//	left or right
-	int				__nil;				//	number of nil under node
 	int				__depth;			//	hight of this node
 }	t_data;
 
 
-int nilNumber (node_pointer node)
-{
-    if(node == nullptr)
-        return 1; 
-    if(node->__left_ == nullptr && node->__right_ == nullptr) 
-        return 2;         
-    else if(node->__left_ != nullptr && node->__right_ == nullptr) 
-        return 1;
-    else if(node->__left_ == nullptr && node->__right_ != nullptr) 
-        return 1;         
-    else
-        return nilNumber(node->__left_) + nilNumber(node->__right_); 
-}
-
 void getInfo(node_pointer node, std::vector<t_data> &data)
 {
 	static int i;
+	t_data info;
+
 	if (node)
 	{
-		t_data info;
+
 		info.__node = node;
 		if (node->__parent_->__parent_ == nullptr)
 			info.__direction = 'r';
-		else if (node == node->__parent_->__left_)
+		else if (ft::__tree_is_left_child(node))
 			info.__direction = '<';
-		else if (node == node->__parent_->__right_)
+		else
 			info.__direction = '>';
-		// info.__nil = 0;
-		info.__nil = nilNumber(node);
 		info.__depth = ++i;
 		data.push_back(info);
-	
+
+		node->__passed_ = false;
+		
 		if (node->__left_)
 			getInfo(node->__left_, data);
 		else if (node->__right_)
 			getInfo(node->__right_, data);
 		else 
 		{
-			if (node == node->__parent_->__left_)
+			while (node->__parent_ && !node->__passed_ && (!node->__right_ || !node->__right_->__passed_))
 			{
 				node = node->__parent_;
 				i--;
 			}
-			else
-			{
-				while (node == node->__parent_->__right_)
-				{
-					node = node->__parent_;
-					i--;
-				}
-				node = node->__parent_;i--;
-			}
-			if (node->__right_)
+			if (node->__right_ && node->__parent_)
 				getInfo(node->__right_, data);
-			else
-				getInfo(node->__parent_, data);
 		}
 	}
 }
@@ -103,15 +81,6 @@ void display(ft::map<int, char> &__m)
 
 	getInfo(root, data);
 	sort(data.begin(), data.end(), compareByfirst);
-	std::cout << "===========================\n";
-	for (std::vector<t_data>::iterator x = data.begin(); x != data.end(); x++)
-	{
-		std::cout << "depth = " << x->__depth << '\t' <<
-		"direction = " << x->__direction << '\t' <<
-		"node.value.first = " << x->__node->__value_.first << '\t' <<
-		"nil = " << x->__nil << '\n';
-	}
-	std::cout << "===========================\n";
 	bool r = false;
 	int depth = 0;
 	for (std::vector<t_data>::iterator x = data.begin(); x != data.end(); x++)
@@ -157,5 +126,3 @@ void display(ft::map<int, char> &__m)
 		std::cout << "\n";
 	}	
 }	
-
-	
