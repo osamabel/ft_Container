04e3486760fcce2ee7a879f3d2d41aa19702b60a
oussama belkhadir <osamaalpha1997@gmail.com>
2023-01-05 11:32:22 +0100

    modifie iterator

--- iterators/iterator.hpp
+++ iterators/iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: obelkhad <obelkhad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 17:43:58 by obelkhad          #+#    #+#             */
-/*   Updated: 2022/11/30 18:09:29 by obelkhad         ###   ########.fr       */
+/*   Updated: 2023/01/05 11:27:37 by obelkhad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,15 @@ namespace ft
 		typedef typename ft::iterator_traits<iterator_type>::pointer				pointer;
 		typedef typename ft::iterator_traits<iterator_type>::reference				reference;
 
+	//+------------------------------------------------------------------------+
+	//|																		   |
+	//|			 		           Move iterator	    					   |
+	//|																		   |
+	//+------------------------------------------------------------------------+
 		//+--------------------------------------------------------------------+
 		//|					           base()     							   |
 		//+--------------------------------------------------------------------+
 		iterator_type base() const { return (this->__i); }
-
 
 		//+--------------------------------------------------------------------+
 		//|					         Constructors                              |
@@ -41,16 +45,13 @@ namespace ft
 		base_Iterator()
 		{}
 
-		// copy-constructible,
- 		template <class _Up> 
-		base_Iterator(const base_Iterator<_Up> &__u) : __i((iterator_type)__u.base())
-		{}
-	
-		base_Iterator(const base_Iterator &__x): __i(__x.base())
-		{}
-
 		// parameterized-constructible,
-		base_Iterator(const iterator_type &__x): __i(__x) 
+		base_Iterator(iterator_type __x): __i(__x) 
+		{}
+		
+		// copy-constructible,
+ 		template <class U> 
+		base_Iterator(const base_Iterator<U> &__u) : __i((iterator_type)__u.base())
 		{}
 
 		// copy-assignable 
@@ -70,8 +71,7 @@ namespace ft
 		//+-------------------------------------------------------------------+
 		reference operator * () const { return *__i; }
 
-		// pointer  operator -> () const { return __i; }
-		pointer  operator -> () const { return std::addressof(operator*()); }   //return (pointer)_VSTD::addressof(*__i);
+		pointer  operator -> () const { return  std::addressof(operator*()); }
 
 		//+-------------------------------------------------------------------+
 		//|					         Incrementation						  	  |
@@ -117,56 +117,13 @@ namespace ft
 			return this->__i + __n;
 		}
 		
-		difference_type operator + (const base_Iterator &iter)
-		{
-			return (base() + iter.base());
-		}
-		
-		base_Iterator  operator - (difference_type __n) const
+		base_Iterator operator - (difference_type __n) const
         {
 			return this->__i - __n;
 		}
-		
-		difference_type operator - (const base_Iterator &iter)
-		{
-			return (base() - iter.base());
-		}
-		//+-------------------------------------------------------------------+
-		//|					          Comparisons							  |
-		//+-------------------------------------------------------------------+
+
 		//[inline] keyword simply include the funtion body in where it called, 
 		//to avoid multipl jmp form main to this function for exmple.
-
-		inline bool operator == (const base_Iterator &__x)
-		{
-			return this->base() == __x.base();
-		}
-
-		inline bool operator != (const base_Iterator &__x)
-		{
-			return this->base() != __x.base();
-		}
-
-		inline bool operator < (const base_Iterator &__x)
-		{
-			return this->base() < __x.base();
-		}
-	
-		inline bool operator <= (const base_Iterator &__x)
-		{
-			return this->base() <= __x.base();
-		}
-
-		inline bool operator > (const base_Iterator &__x)
-		{
-			return this->base() > __x.base();
-		}
-		
-		inline bool operator >= (const base_Iterator &__x)
-		{
-			return this->base() >= __x.base();
-		}
-
 		//+-------------------------------------------------------------------+
 		//|				     Compound Assignment Operators			 		  |
 		//+-------------------------------------------------------------------+
@@ -176,7 +133,7 @@ namespace ft
 			return *this;
 		}
 
-		base_Iterator& operator -= (difference_type __n)
+		base_Iterator &operator -= (difference_type __n)
         {
 			__i -= __n;
 			return *this;
@@ -191,169 +148,51 @@ namespace ft
 		}
 	private:
     	iterator_type __i;
+	
 	};
-
-
-	template <class Iter>
-	class reverse_iterator
-	{
-		typedef Iter                                              				iterator_type;
-		typedef typename iterator_traits<iterator_type>::difference_type		difference_type;
-		typedef typename iterator_traits<iterator_type>::value_type				value_type;
-		typedef typename iterator_traits<iterator_type>::pointer				pointer;
-		typedef typename iterator_traits<iterator_type>::reference				reference;
-
-	public:
-		//+-------------------------------------------------------------------+
-		//|					          Constructors							  |
-		//+-------------------------------------------------------------------+
-		reverse_iterator() :  current() {}
-		explicit reverse_iterator(iterator_type __x) : current(__x) {}
-		// template <class _Up> reverse_iterator(const reverse_iterator<_Up>& __u) : __t(__u.base()), current(__u.base()) {}
-		// template <class _Up> reverse_iterator& operator=(const reverse_iterator<_Up>& __u)
-		// {
-		// 	__t = current = __u.base();
-		// 	return *this;
-		// }
-		//+-------------------------------------------------------------------+
-		//|					          base()     							  |
-		//+-------------------------------------------------------------------+
-		iterator_type base() const { return current; }
-		
-		//+-------------------------------------------------------------------+
-		//|					    Input::Dereferenceable	 		  	  	      |
-		//+-------------------------------------------------------------------+
-		reference operator * () const 
-		{
-			iterator_type __tmp = current;
-			return *--__tmp;
-		}
-		pointer  operator -> () const 
-		{
-			return _VSTD::addressof(operator*());
-		}
-		
-		//+-------------------------------------------------------------------+
-		//|					         Incrementation						  	  |
-		//+-------------------------------------------------------------------+
-		reverse_iterator &operator ++ ()
-		{
-			--current;
-			return *this;
-		}
-		reverse_iterator  operator ++ (int) 
-		{
-			reverse_iterator __tmp(*this);
-			--current; return __tmp;
-		}
-
-		//+-------------------------------------------------------------------+
-		//|					         Decrementation						  	  |
-		//+-------------------------------------------------------------------+
-		reverse_iterator &operator -- ()
-		{
-			++current;
-			return *this;
-		}
-		reverse_iterator  operator -- (int)
-		{
-			reverse_iterator __tmp(*this);
-			++current;
-			return __tmp;
-		}
-
-		//+-------------------------------------------------------------------+
-		//|					          Arithmetic						  	  |
-		//+-------------------------------------------------------------------+
-		reverse_iterator  operator + (difference_type __n) const
-		{
-			return reverse_iterator(current - __n);
-		}
-			
-		reverse_iterator  operator - (difference_type __n) const
-		{
-			return reverse_iterator(current + __n);
-		}
-
-
-		//+-------------------------------------------------------------------+
-		//|				     Compound Assignment Operators			 		  |
-		//+-------------------------------------------------------------------+
-		reverse_iterator &operator += (difference_type __n)
-		{
-			current -= __n;
-			return *this;
-		}
-		
-		reverse_iterator &operator -= (difference_type __n)
-		{
-			current += __n;
-			return *this;
-		}
-
-		//+-------------------------------------------------------------------+
-		//|					          Operator []							  |
-		//+-------------------------------------------------------------------+
-		reference operator [] (difference_type __n) const
-		{
-			return *(*this + __n);
-		}
-	private:
-    	Iter current;
-	};
-
-
-//-------------------------------------------------------[Non-member functions ]
 
 /*----------------------------------------------------------------------------*/
 /*                      Compares the underlying iterators                     */
 /*----------------------------------------------------------------------------*/
-template <class Iter1, class Iter2>
-inline bool operator == (const reverse_iterator<Iter1> &__x, const reverse_iterator<Iter2> &__y)
-{return __x.base() == __y.base();}
 
+	template <class Iter1, class Iter2>
+	bool operator == (const base_Iterator<Iter1> &__x, const base_Iterator<Iter2> &__y)
+	{return __x.base() == __y.base();}
+	
+	template <class Iter1, class Iter2>
+	bool operator != (const base_Iterator<Iter1> &__x, const base_Iterator<Iter2> &__y)
+	{return __x.base() != __y.base();}
+	template <class Iter1, class Iter2>
+	inline bool operator < (const base_Iterator<Iter1> &__x, const base_Iterator<Iter2> &__y)
+	{return __x.base() < __y.base();}
 
-template <class Iter1, class Iter2>
-inline bool operator != (const reverse_iterator<Iter1> &__x, const reverse_iterator<Iter2> &__y)
-{return __x.base() != __y.base();}
+	template <class Iter1, class Iter2>
+	inline bool operator <= (const base_Iterator<Iter1> &__x, const base_Iterator<Iter2> &__y)
+	{return __x.base() <= __y.base();}
 
+	template <class Iter1, class Iter2>
+	inline bool operator > (const base_Iterator<Iter1> &__x, const base_Iterator<Iter2> &__y)
+	{return __x.base() > __y.base();}
 
-template <class Iter1, class Iter2>
-inline bool operator < (const reverse_iterator<Iter1> &__x, const reverse_iterator<Iter2> &__y)
-{return __x.base() > __y.base();}
+	template <class Iter1, class Iter2>
+	inline bool operator >= (const base_Iterator<Iter1> &__x, const base_Iterator<Iter2> &__y)
+	{return __x.base() >= __y.base();}
 
+	/*----------------------------------------------------------------------------*/
+	/*                      operator+/-(std::reverse_iterator)                    */
+	/*----------------------------------------------------------------------------*/
+	
+	template <class Iterator>  
+	base_Iterator<Iterator> operator + (typename base_Iterator<Iterator>::difference_type __n, const base_Iterator<Iterator> &__x)
+	{
+		return __x.base() + __n; 
+	}
 
-template <class Iter1, class Iter2>
-inline bool operator <= (const reverse_iterator<Iter1> &__x, const reverse_iterator<Iter2> &__y)
-{return __x.base() >= __y.base();}
+	template <class Iterator1, class Iterator2>
+	typename base_Iterator<Iterator1>::difference_type 
+	operator - (const base_Iterator<Iterator1> &__x,  const base_Iterator<Iterator2> &__y)
+	{ return __x.base() - __y.base(); }
 
-
-template <class Iter1, class Iter2>
-inline bool operator > (const reverse_iterator<Iter1> &__x, const reverse_iterator<Iter2> &__y)
-{return __x.base() < __y.base();}
-
-
-template <class Iter1, class Iter2>
-inline bool operator >= (const reverse_iterator<Iter1> &__x, const reverse_iterator<Iter2> &__y)
-{return __x.base() <= __y.base();}
-
-/*----------------------------------------------------------------------------*/
-/*                      operator-(std::reverse_iterator)                      */
-/*----------------------------------------------------------------------------*/
-template <class _Iter1, class _Iter2>
-inline typename reverse_iterator<_Iter1>::difference_type
-operator - (const reverse_iterator<_Iter1>& __x, const reverse_iterator<_Iter2>& __y)
-{
-    return __y.base() - __x.base();
-}
-
-/*----------------------------------------------------------------------------*/
-/*                      operator+(std::reverse_iterator)                      */
-/*----------------------------------------------------------------------------*/
-template <class Iter>
-inline reverse_iterator<Iter>
-operator + (typename reverse_iterator<Iter>::difference_type __n, const reverse_iterator<Iter> &__x)
-{return reverse_iterator<Iter>(__x.base() - __n);}
 }	//namespace ft
 
 #endif
